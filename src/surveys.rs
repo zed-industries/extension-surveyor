08a5847bef5ed108b5667c42581440c0b29e9ed1
Marshall Bowers <git@maxdeviant.com>
2025-02-23 15:15:17 -0500

    Tweak the report format

--- src/surveys.rs
+++ src/surveys.rs
@@ -47,6 +47,25 @@ impl ThemesUsingProperty {
                 }
             };
 
+            fn write_extension_header(
+                report: &mut Vec<u8>,
+                extension_id: &str,
+                extension_manifest: &ExtensionManifest,
+            ) -> Result<()> {
+                writeln!(report, "- [ ] `{extension_id}`")?;
+                write!(report, "  - Repository: ")?;
+                if let Some(repository) = extension_manifest.repository.as_ref() {
+                    writeln!(report, "[{repository}]({repository})")?;
+                } else {
+                    writeln!(report, "???")?;
+                }
+
+                writeln!(report, "  - Issue: TBD")?;
+                writeln!(report, "  - Errors:")?;
+
+                Ok(())
+            }
+
             let mut themes = Vec::new();
 
             let mut themes_entries = fs::read_dir(&themes_dir).await?;
@@ -64,18 +83,10 @@ impl ThemesUsingProperty {
                 ) {
                     Ok(theme) => theme,
                     Err(err) => {
-                        writeln!(&mut report, "### {extension_id}")?;
-                        write!(&mut report, "Repository: ")?;
-                        if let Some(repository) = extension_manifest.repository.as_ref() {
-                            writeln!(&mut report, "[{repository}]({repository})")?;
-                        } else {
-                            writeln!(&mut report, "???")?;
-                        }
-
-                        writeln!(&mut report, "Issue: ")?;
+                        write_extension_header(&mut report, extension_id, &extension_manifest)?;
                         writeln!(
                             &mut report,
-                            "- Failed to parse theme file at {theme_path:?}: {err}"
+                            "    - Failed to parse theme file at {theme_path:?}: {err}"
                         )?;
 
                         continue;
@@ -90,22 +101,13 @@ impl ThemesUsingProperty {
             for theme in themes {
                 if theme.style.contains_key(&self.theme_property) {
                     if !wrote_header {
-                        writeln!(&mut report, "### {extension_id}")?;
-                        write!(&mut report, "Repository: ")?;
-                        if let Some(repository) = extension_manifest.repository.as_ref() {
-                            writeln!(&mut report, "[{repository}]({repository})")?;
-                        } else {
-                            writeln!(&mut report, "???")?;
-                        }
-
-                        writeln!(&mut report, "Issue: ")?;
-
+                        write_extension_header(&mut report, extension_id, &extension_manifest)?;
                         wrote_header = true;
                     }
 
                     writeln!(
                         &mut report,
-                        "- Theme {:?} is using deprecated style property `{}`",
+                        "    - Theme {:?} is using deprecated style property `{}`",
                         theme.name, self.theme_property
                     )?;
                 }

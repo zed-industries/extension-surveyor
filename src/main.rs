9137d231075e1d16825633da4ce7de29f457f0ac
Marshall Bowers <git@maxdeviant.com>
2025-02-23 13:43:36 -0500

    Move repository cloning to a subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,42 @@
 use std::path::PathBuf;
 
 use anyhow::{Context as _, Result};
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use tokio::fs;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
-struct Cli {}
+struct Cli {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Updates the extensions repository with the latest changes.
+    UpdateRepo,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let _cli = Cli::parse();
+    let cli = Cli::parse();
 
     let extension_repository_url = "https://github.com/zed-industries/extensions.git";
     let work_dir = PathBuf::from("work");
 
-    fs::create_dir_all(&work_dir).await?;
+    match cli.command {
+        Command::UpdateRepo => {
+            fs::create_dir_all(&work_dir).await?;
 
-    tokio::process::Command::new("git")
-        .args(["clone", "--recurse-submodules", &extension_repository_url])
-        .arg(&work_dir)
-        .spawn()?
-        .wait()
-        .await
-        .context("failed to clone extensions repository")?;
+            tokio::process::Command::new("git")
+                .args(["clone", "--recurse-submodules", &extension_repository_url])
+                .arg(&work_dir)
+                .spawn()?
+                .wait()
+                .await
+                .context("failed to clone extensions repository")?;
 
-    Ok(())
+            Ok(())
+        }
+    }
 }

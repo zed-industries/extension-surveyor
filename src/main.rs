38f03aac19473af99ea316513937e90aa9013eea
Marshall Bowers <git@maxdeviant.com>
2025-02-23 13:39:50 -0500

    Clone down the extensions repository

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use anyhow::Result;
+use std::path::PathBuf;
+
+use anyhow::{Context as _, Result};
 use clap::Parser;
+use tokio::fs;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -8,6 +11,19 @@ struct Cli {}
 #[tokio::main]
 async fn main() -> Result<()> {
     let _cli = Cli::parse();
+
+    let extension_repository_url = "https://github.com/zed-industries/extensions.git";
+    let work_dir = PathBuf::from("work");
+
+    fs::create_dir_all(&work_dir).await?;
+
+    tokio::process::Command::new("git")
+        .args(["clone", "--recurse-submodules", &extension_repository_url])
+        .arg(&work_dir)
+        .spawn()?
+        .wait()
+        .await
+        .context("failed to clone extensions repository")?;
 
     Ok(())
 }
